e3e8888da7483681558b1dba88356b3d661d76c2
Vlad Dziuba <dzyubavlad@gmail.com>
2019-12-04 15:50:00 +0200

    (1) the game is on

---
+++ 1/1.hs
@@ -0,0 +1,11 @@
+fuel :: Int -> Int
+fuel mas = max 0 $ div mas 3 - 2
+
+fullFuel :: Int -> Int
+fullFuel mas = sum $ takeWhile (> 0) $ drop 1 $ iterate fuel mas
+
+main :: IO ()
+main = do
+  ms <- map read . lines <$> readFile "1.in"
+  print $ sum $ map fuel ms
+  print $ sum $ map fullFuel ms
