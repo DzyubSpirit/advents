08aafcc102041ad89269b3fc3296bc537b4fbad9
Vlad Dziuba <dzyubavlad@gmail.com>
2020-12-04 00:20:05 +0000

    Solve 3 day

---
+++ 2020/3/star.hs
@@ -0,0 +1,37 @@
+import Control.Monad (when)
+import qualified Data.Array as A
+import Data.Bifunctor (bimap)
+import System.Environment (getArgs)
+
+type Height = Int
+type Width  = Int
+
+data TreeMap = TreeMap 
+  { height :: Height
+  , width :: Width 
+  , arr   :: A.Array (Int, Int) Char
+  }
+
+fromString :: String -> TreeMap
+fromString str = TreeMap height width 
+               $ A.listArray ((0, 0), (height-1, width-1)) 
+               $ filter (/= '\n') str
+  where width  = length $ head $ lines str
+        height = length str `div` (width+1)
+
+(!) :: TreeMap -> (Int, Int) -> Char
+(TreeMap _ width arr) ! (y, x) =  arr A.! (y, x `mod` width)
+
+numTreesOnSlope :: TreeMap -> (Int, Int) -> Int
+numTreesOnSlope treeMap (dy, dx) = length $ filter (== '#') $ map (treeMap !)
+                                 $ takeWhile ((< height treeMap) . fst)
+                                 $ iterate (bimap (+ dy) (+ dx)) (0, 0)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  when (length args /= 1) $
+    fail $ "Expected one command line argument, got " ++ show args
+  treeMap <- fromString <$> readFile (head args)
+  print $ product $ map (numTreesOnSlope treeMap) 
+    [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
