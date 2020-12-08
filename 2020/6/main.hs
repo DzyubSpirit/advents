b3f347354501276c5dcb8abef687dd7cc71d9f49
Vlad Dziuba <dzyubavlad@gmail.com>
2020-12-08 22:55:43 +0000

    Solve 6 day

---
+++ 2020/6/main.hs
@@ -0,0 +1,14 @@
+import Control.Monad (when)
+import System.Environment (getArgs)
+
+import Data.List.Split (splitOn)
+
+import qualified Data.Set as S
+
+main :: IO ()
+main = do
+  args <- getArgs
+  when (length args /= 1) $
+    fail $ "Expected one command line argument, got " ++ show args
+  answers <- map lines . splitOn "\n\n" <$> readFile (head args)
+  print $ sum $ map (S.size . S.fromList . concat) answers
