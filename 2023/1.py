3d9ffa3fa5d6cf971480c9afb2183a218208eece
Vlad Dziuba <dzyubavlad@gmail.com>
2023-12-08 21:01:28 +0000

    2023: solve day 1

---
+++ 2023/1.py
@@ -0,0 +1,12 @@
+with open('sample_data/vlad_1_example.txt') as f:
+    lines = f.readlines()
+
+sum = 0
+for line in lines:
+    digits = [ch for ch in line if ch >= '0' and ch <= '9']
+    # digits = []
+    # for ch in line:
+    #   if ch >= '0' and ch <= '9':
+    #     digits.append(ch)
+    sum += int(digits[0])*10 + int(digits[-1])
+print(sum)
